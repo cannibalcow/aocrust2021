40133c7e27c47f8005a3da8e5ff667a004e96225
Daniel Heldt <danielheldtsmail@gmail.com>
2022-01-05 12:42:06 +0100

    day 4 p1

--- src/days/day4/day4.rs
+++ src/days/day4/day4.rs
@@ -4,35 +4,24 @@ use crate::tools;
 use colored::Colorize;
 
 pub fn solve_part1() {
-    let input_path = "D04_input_test.txt";
+    tools::pretty_print_day(4, 1);
+    println!("Giant Squid");
+    let input_path = "D04_input.txt";
     let numbers = parse_input_to_numbers(&input_path);
     let mut boards = parse_input_to_boards(&input_path);
 
-    println!("{:?}", numbers);
-
-    println!("== Boards");
-
     for num in &numbers {
-        println!("\n Marking {}", num);
         for board in &mut boards {
             board.mark(num.clone());
-            board.print_board();
-            println!("Bingo: {}", board.has_bingo());
-
-            if board.has_bingo() {
+            if board.has_column_bingo() || board.has_row_bingo() {
+                println!("BINGO!");
+                board.print_board();
                 let sum = board.sum_of_all_unmarked();
-                println!("Sum: {}", sum);
-                println!("Num: {}", num);
-                println!("Reuslt: {}", sum * num);
                 tools::pretty_print_result((num * sum) as i32);
                 return;
             }
         }
     }
-
-    // for board in boards {
-    // board.print_board();
-    // }
 }
 
 pub fn solve_part2() {}
@@ -78,7 +67,6 @@ fn parse_input_to_boards(path: &str) -> Vec<BingoBoard> {
 
 fn parse_input_to_numbers(path: &str) -> Vec<u32> {
     let lines = tools::read_file_as_string(path);
-    println!("Lines {}", lines.len());
     let numbers = lines[0]
         .split(",")
         .map(|f| f.parse::<u32>().unwrap())
@@ -108,7 +96,33 @@ impl BingoBoard {
             .sum::<u32>();
     }
 
-    fn has_bingo(&self) -> bool {
+    fn has_column_bingo(&self) -> bool {
+        let rows: Vec<&[u32]> = self.numbers.chunks(5).collect();
+        let mut columns = vec![];
+        for col in 0..5 {
+            let mut c: Vec<&u32> = vec![];
+            for row in 0..5 {
+                c.push(&rows[row][col])
+            }
+            columns.push(c.clone());
+        }
+
+        for col in columns {
+            let mut count = 0;
+            for i in col {
+                if self.marked_numbers.contains(i) {
+                    count += 1;
+                }
+            }
+            if count == 5 {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    fn has_row_bingo(&self) -> bool {
         let rows: Vec<&[u32]> = self.numbers.chunks(5).collect();
 
         for row in rows {
@@ -126,7 +140,6 @@ impl BingoBoard {
     }
 
     fn print_board(&self) {
-        println!("");
         for (i, value) in self.numbers.iter().enumerate() {
             let is_marked = self.marked_numbers.contains(value);
 
@@ -145,7 +158,7 @@ impl BingoBoard {
                 }
             }
         }
-        println!("");
+        println!("\n");
     }
 
     fn mark(&mut self, num: u32) {
